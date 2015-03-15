98f61fb67cbb1791fea9855b1e1996add0ce2e21
Elie Schoppik <eschoppik@gmail.com>
2015-03-15 14:52:37 -0700

    adding another solution

--- sort.js
+++ sort.js
@@ -1,23 +1,41 @@
+
+
+// function bubbleSort(arr){
+// // iterative
+//   var array_length = arr.length,
+//     swapped,
+//     swap;
+
+//   while (swapped === false) {
+//     swapped = false;
+//     for(var i=0 ; i < arr.length; i++) {
+//       if (arr[i] > arr[i+1]) {
+//         swap = arr[i];
+//         arr[i] = arr[i+1];
+//         arr[i+1] = swap;
+//         swapped = true;
+//       }
+//     }
+//   }
+
+//   return arr;
+// }
+
 function bubbleSort(arr){
-// iterative
-  var array_length = arr.length,
-    swapped,
-    swap;
-
-  do {
-    swapped = false;
-    for(var i=0 ; i < array_length; i++) {
-      if (arr[i] > arr[i+1]) {
-        swap = arr[i];
-        arr[i] = arr[i+1];
-        arr[i+1] = swap;
-        swapped = true;
-      }
+    var temp;
+    for (var i=0; i < arr.length-1; i++){
+        for (var j=1; j < arr.length-i; j++){
+            if (arr[j] < arr[j-1]){
+                temp = arr[j-1]
+                arr[j-1] = arr[j]
+                arr[j] = temp
+            }
+        }
     }
-  } while (swapped === true);
 
-  return arr;
+    return arr;
 }
+
 
 // do not touch this
 module.exports = bubbleSort
